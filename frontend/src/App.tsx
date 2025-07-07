6b77d5e17be330a2ce36269a30f1d6096c91ba12
OmarrGhorab <omarghorabdev@gmail.com>
2025-07-07 20:12:20 +0300

    fix cors and update ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { Container, Stack } from "@chakra-ui/react"
 import TodoForm from "./components/TodoForm"
 import TodoList from "./components/TodoList"
+import Navbar from "./components/Navbar"
 
-
-
+export const BASE_URL = "http://localhost:4000/api";
 function App() {
   return (
     <Stack h="100vh">
+      <Navbar />
       <Container>
         <TodoForm />
         <TodoList />
